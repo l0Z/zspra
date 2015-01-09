cca9f0b592ba9bcb73b11ccddc88f584794d2f3f
1010 <zhaoshi1991@qq.com>
2015-01-09 11:05:32 +0800

    add sh
    ~

---
+++ para_constructKB.sh
@@ -0,0 +1,15 @@
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb0' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb1' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb2' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb3' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb4' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb5' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb6' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb7' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb8' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb9' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb10' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb11' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb12' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb13' &&
+python /sigir15/constructKB.py '/home/zhaoshi/文档/topicdata/topicfb14'
